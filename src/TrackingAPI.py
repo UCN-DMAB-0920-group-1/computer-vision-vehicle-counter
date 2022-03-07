84bd18b87f194c7f888970f818ce42c6f388735c
Magnus Baggesen <baggesenmagnus@gmail.com>
2022-03-07 10:27:35 +0100

    Flask init

---
+++ src/TrackingAPI.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify, flash, request, redirect, url_for
+from dummydata import dummydata
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+app.secret_key = "super secret key"
+test = dummydata()
+UPLOAD_FOLDER = './sources'
+ALLOWED_EXTENSIONS = {'mp4'}
+app.config[UPLOAD_FOLDER] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(UPLOAD_FOLDER, filename)
+            return redirect(url_for('download_file', name=filename))
+    return 'lolz'
