01f33a5acd5c8c1584e13aaebf437aa089b6c4ef
Magnus Baggesen <baggesenmagnus@gmail.com>
2022-03-09 13:25:58 +0100

    MongoDB connected

---
+++ API/src/dao/daoDetections.py
@@ -0,0 +1,26 @@
+from matplotlib.collections import Collection
+from interfaces.IDao import IDao
+from pymongo import MongoClient
+import pymongo
+from ...env import CONNECTION_STRING
+
+
+class daoDetections(IDao):
+
+    global collection
+    client = MongoClient(CONNECTION_STRING)
+    db = client['AI_result_database']
+    collection = db['Vehicle_tracking_result']
+
+    def find_one(id: int):
+        res = collection.find_one({"_id": id})
+        return res
+
+    def insert_one(id: int, object):
+        video = id + '.mp4'
+        res = collection.insert_one({
+            "_id": id,
+            "video": video,
+            "detections": object['total']
+        })
+        return res
