e078804c94bf882d1f7011e6ed135f928ea3f5ac
MasterMidi <michael-graversen@hotmail.com>
2022-03-14 22:47:21 +0100

    defines ROI, crops selection, overlay on origin

--- api/src/tracking_module/tracking.py
+++ api/src/tracking_module/tracking.py
@@ -6,9 +6,6 @@ import numpy as np
 import pandas
 
 from norfair import Tracker
-from importlib.resources import path
-from charset_normalizer import detect
-from matplotlib.pyplot import draw
 
 from tracking_module.util import get_stream, centroid
 from tracking_module.norfair_helpers import euclidean_distance, yolo_detections_to_norfair_detections
@@ -33,7 +30,6 @@ class Tracking:
         self.model = torch.hub.load(
             repo_or_dir="ultralytics/yolov5", model=detector_path)
         self.model.conf = confidence_threshold
-        #self.model.
 
         self.inside_roi = []  # Int array
         self.vehicle_count = 0
@@ -137,12 +133,45 @@ class Tracking:
             if(ret is False):
                 break
 
+            # mask defaulting to black for 3-channel and transparent for 4-channel
+            # (of course replace corners with yours)
+            mask = np.zeros(frame.shape, dtype=np.uint8)
+            roi_corners = np.array([[(0,250), (520,90), (640,90), (640,719), (0,719)]], dtype=np.int32)
+
+            # automatically find lowest offsets
+            min_x = min(p[0] for p in roi_corners)[0]
+            min_y = min(p[1] for p in roi_corners)[1]
+            roi_offset = (min_x,min_y)
+
+
+            # fill the ROI so it doesn't get wiped out when the mask is applied
+            channel_count = frame.shape[2]  # channel count on image
+            ignore_mask_color = (255,)*channel_count # array of white color, sized to channels count
+
+            # draw desired area on mask
+            cv2.fillConvexPoly(mask, roi_corners, ignore_mask_color)
+            #cv2.drawContours(mask, [roi_corners], -1, ignore_mask_color, -1, cv2.LINE_AA)
+
+            # apply the mask
+            masked_image = cv2.bitwise_and(frame, mask)
+            cv2.imwrite('image_masked.png', masked_image)
+            cv2.imwrite('image.png', frame)
+
+            # crop frame to masked area
+            b_rect = cv2.boundingRect(roi_corners) # returns (x,y,w,h) of the rect
+            cropped_image = masked_image[b_rect[1]: b_rect[1] + b_rect[3], b_rect[0]: b_rect[0] + b_rect[2]]
+
+            comb_image = cv2.addWeighted(frame, 1, masked_image, 0, 0)
+            cv2.imwrite('image_overlayed.png', comb_image)
+            cv2.imwrite('image_cropped.png', cropped_image)
+
             # Detect objects inside the frame
-            yolo_detections = self.model(frame)
+            #yolo_detections = self.model(frame)
+            yolo_detections = self.model(cropped_image)
 
             # Convert to norfair detections
-            detections = yolo_detections_to_norfair_detections(
-                yolo_detections, track_points=self.track_points)
+            #detections = yolo_detections_to_norfair_detections(yolo_detections, track_points=self.track_points)
+            detections = yolo_detections_to_norfair_detections(yolo_detections, track_points=self.track_points, offset=roi_offset)
 
             # Update tracker
             tracked_objects = self.tracker.update(detections=detections)
@@ -167,8 +196,8 @@ class Tracking:
                 print(self.inside_roi)
 
             if(self.should_draw):
-                self.draw(frame, yolo_detections,
-                          detections, tracked_objects)
+                self.draw(frame, yolo_detections, detections, tracked_objects)
+                #self.draw(cropped_image, yolo_detections, detections, tracked_objects)
             # Wait for ESC to be pressed (then exit)
             if(cv2.waitKey(10) == 27):
                 break
