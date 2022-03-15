009a60597f944c4731fb5e52c3a5054581bde942
Julius Bendt <julius@juto.dk>
2022-03-15 13:02:03 +0100

    Initial UI

--- frontend/src/components/home/FileUpload.vue
+++ frontend/src/components/home/FileUpload.vue
@@ -1,23 +1,35 @@
 <template>
-  <div class="bg-blue-200 rounded-xl p-3">
-    <h1>Upload video</h1>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum sint
-      accusamus aliquam commodi quisquam beatae tempore vitae quo iste sit!
-      Cumque ducimus distinctio pariatur doloremque reiciendis repellat amet sed
-      iste?
-    </p>
-    <input
-      class="block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100"
-      type="file"
-      @change="onFileChange"
-    />
-    <button
-      class="block w-full rounded-full bg-blue-300 p-2 text-white mt-4"
-      @click="onUploadFile"
-    >
-      Upload
-    </button>
+  <div>
+    <section v-if="loading">
+      <div class="flex justify-center items-center">
+        <div
+          class="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full"
+          role="status"
+        >
+          <span class="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    </section>
+    <section class="bg-blue-200 rounded-xl p-3" v-else>
+      <h1>Upload video</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum sint
+        accusamus aliquam commodi quisquam beatae tempore vitae quo iste sit!
+        Cumque ducimus distinctio pariatur doloremque reiciendis repellat amet
+        sed iste?
+      </p>
+      <input
+        class="block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100"
+        type="file"
+        @change="onFileChange"
+      />
+      <button
+        class="block w-full rounded-full bg-blue-300 p-2 text-white mt-4"
+        @click="onUploadFile"
+      >
+        Upload
+      </button>
+    </section>
   </div>
 </template>
 
--- frontend/src/components/home/VideoResult.vue
+++ frontend/src/components/home/VideoResult.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="bg-blue-200 rounded-xl p-3">
-    <h1>Upload video</h1>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum sint
-      accusamus aliquam commodi quisquam beatae tempore vitae quo iste sit!
-      Cumque ducimus distinctio pariatur doloremque reiciendis repellat amet sed
-      iste?
+    <h1>Video result</h1>
+    <p v-for="vehicle in vehicleTypes" :key="vehicle.type">
+      {{ vehicle.type }}: {{ vehicle.amount }}
     </p>
-    <input
-      class="block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100"
-      type="file"
-    />
+    <p>Total vehicles: {{ totalCars }}</p>
+
     <button class="block w-full rounded-full bg-blue-300 p-2 text-white mt-4">
-      Upload
+      Download visualized file
     </button>
   </div>
 </template>
+
+<script>
+import { ref, computed } from "vue";
+
+export default {
+  setup() {
+    const vehicleTypes = ref([
+      {
+        type: "car",
+        amount: 10,
+      },
+      {
+        type: "truck",
+        amount: 23,
+      },
+      {
+        type: "bus",
+        amount: 2,
+      },
+    ]);
+
+    const totalCars = computed(() => {
+      return vehicleTypes.value.reduce((total, item) => item.amount + total, 0);
+    });
+
+    return {
+      vehicleTypes,
+      totalCars,
+    };
+  },
+};
+</script>
