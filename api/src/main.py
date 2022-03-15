64b42a750a94dd8e379c13b6c1129235a76e3836
MasterMidi <michael-graversen@hotmail.com>
2022-03-15 12:54:41 +0100

    use defaults

--- api/src/main.py
+++ api/src/main.py
@@ -1,7 +1,7 @@
 from tracking_module.tracking import Tracking
 from tracking_module.streams import streams
 
-tracker = Tracking(should_draw=True, custom_model=True, model_path="./models/yolov5m-custom-97.pt",
+tracker = Tracking(should_draw=True,  # custom_model=True, model_path="./models/yolov5m-custom-97.pt",
                    roi_area=[[(0, 250), (520, 90), (640, 90), (640, 719), (0, 719)]])
 detections = tracker.track(streams["file-5-sec"])
 
