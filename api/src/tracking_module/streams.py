d8c3c5142d04e94930b0fe4f298d8575d7f95764
MasterMidi <michael-graversen@hotmail.com>
2022-03-15 13:19:56 +0100

    Update streams.py

--- api/src/tracking_module/streams.py
+++ api/src/tracking_module/streams.py
@@ -1,8 +1,8 @@
 streams = {"highway": "https://www.youtube.com/watch?v=5_XSYlAfJZM",
            "highway2": "https://www.youtube.com/watch?v=ffsC9km5xDY",
            "webcam": "0",
-           "file": "sources/highway-without-the-shakey-part.mp4",
-           "file-5-sec": "sources/highway-5-sec.mp4",
-           "file-shaky": "sources/highway.mp4",
+           "file": "./storage//highway-without-the-shakey-part.mp4",
+           "file-5-sec": "./storage/highway-5-sec.mp4",
+           "file-shaky": "./storage//highway.mp4",
 
            "dr": "https://drlive01hls.akamaized.net/hls/live/2014185/drlive01/master2000.m3u8", }
