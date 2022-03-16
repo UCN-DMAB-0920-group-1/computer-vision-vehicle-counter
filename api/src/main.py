3f3c9114c777135af69869432bffeb3ae4de61df
Michael Andreas Graversen <michael-graversen@hotmail.com>
2022-03-16 22:38:28 +0100

    moved custom model

--- api/src/main.py
+++ api/src/main.py
@@ -2,9 +2,9 @@ from tracking_module import Tracking, streams
 
 tracker = Tracking(should_draw=True,
                    track_points='bbox',
-                   # custom_model=True, model_path="./models/yolov5m-custom-97.pt",
+                   custom_model=True, model_path="./api/models/yolov5m-custom-97.pt",
                    roi_area=[[(0, 250), (520, 90), (640, 90), (640, 719), (0, 719)]])
 
-detections = tracker.track(streams["file"])
+detections = tracker.track(streams["file-5-sec"])
 
 print(detections)
