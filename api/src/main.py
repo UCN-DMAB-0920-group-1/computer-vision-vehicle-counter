a8a2a19caaba75cc1efd647da3cf8563fb2b060f
MasterMidi <michael-graversen@hotmail.com>
2022-03-16 16:25:08 +0100

    Draw bbox (explicit)

--- api/src/main.py
+++ api/src/main.py
@@ -1,6 +1,8 @@
 from tracking_module import Tracking, streams
 
-tracker = Tracking(should_draw=True,  # custom_model=True, model_path="./models/yolov5m-custom-97.pt",
+tracker = Tracking(should_draw=True,
+                   track_points='bbox',
+                   # custom_model=True, model_path="./models/yolov5m-custom-97.pt",
                    roi_area=[[(0, 250), (520, 90), (640, 90), (640, 719), (0, 719)]])
 
 detections = tracker.track(streams["file-5-sec"])
