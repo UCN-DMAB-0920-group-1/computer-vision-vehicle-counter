58ac610d801517669784bdc795f736f3227ea626
MasterMidi <michael-graversen@hotmail.com>
2022-03-16 09:49:09 +0100

    Packaged module

--- api/src/main.py
+++ api/src/main.py
@@ -1,8 +1,8 @@
-from tracking_module.tracking import Tracking
-from tracking_module.streams import streams
+from tracking_module import Tracking, streams
 
 tracker = Tracking(should_draw=True,  # custom_model=True, model_path="./models/yolov5m-custom-97.pt",
                    roi_area=[[(0, 250), (520, 90), (640, 90), (640, 719), (0, 719)]])
+
 detections = tracker.track(streams["file-5-sec"])
 
 print("total cars detected: " + str(detections["total"]))
