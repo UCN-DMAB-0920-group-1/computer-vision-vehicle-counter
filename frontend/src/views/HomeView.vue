9e8986b5b4133402d09d5f2981527d00c4c92ca6
Julius Bendt <julius@juto.dk>
2022-03-17 12:59:50 +0100

    Added bounding box, for a picture

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,13 +1,19 @@
-<template >
-  <div class="grid grid-cols-1 gap-4 mt-10 px-5 sm:grid-cols-2 max-w-5xl mx-auto ">
-    <div class="bg-violet-200 rounded-xl p-3 shadow-xl">
-      <FileUpload></FileUpload>
+<template>
+  <div>
+    <div
+      class="grid grid-cols-1 gap-4 mt-10 px-5 sm:grid-cols-2 max-w-5xl mx-auto"
+    >
+      <div class="bg-violet-200 rounded-xl p-3 shadow-xl">
+        <FileUpload></FileUpload>
+      </div>
+      <VideoResult></VideoResult>
     </div>
-    <VideoResult></VideoResult>
+    <BoundingBox></BoundingBox>
   </div>
 </template>
 
 <script setup>
 import FileUpload from "@/components/home/FileUpload.vue";
 import VideoResult from "@/components/home/VideoResult.vue";
+import BoundingBox from "@/components/home/BoundingBox.vue";
 </script>
