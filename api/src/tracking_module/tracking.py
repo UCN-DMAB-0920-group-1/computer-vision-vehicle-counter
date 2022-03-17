803e211ba233d5de13be55aa010862f8240c7af1
Michael Andreas Graversen <michael-graversen@hotmail.com>
2022-03-17 16:46:54 +0100

    custom label drawing

--- api/src/tracking_module/tracking.py
+++ api/src/tracking_module/tracking.py
@@ -1,6 +1,7 @@
 from cmath import rect
 from collections import namedtuple
 from functools import reduce
+from typing import Iterable
 
 import cv2
 import norfair
@@ -50,7 +51,6 @@ class Tracking:
         self.model.conf = confidence_threshold
         self.model.iou = 0.45
 
-
         self.inside_roi = []  # Int array
         self.detections = {"car": 0}
 
@@ -64,42 +64,49 @@ class Tracking:
         self.roi_area = np.array(roi_area, dtype=np.int32)
         self.should_draw = should_draw
 
-    def draw_label(self, frame, detection, text: str, font_face: int, font_scale: float, font_color: tuple[int, int, int], font_thickness: int):
+    def draw_label(self,
+                   frame,
+                   text: str,
+                   bbox_location: Iterable,
+                   font_face: int = cv2.FONT_HERSHEY_SIMPLEX,
+                   font_scale: float = 0.5,
+                   font_color: tuple[int, int, int] = (255, 255, 255),
+                   font_thickness: int = 1,
+                   box_color: tuple[int, int, int] = (0, 0, 255),
+                   box_margin: int = 2):
+
+        # get size of label to draw
         text_size = cv2.getTextSize(
             text,
             font_face,
             font_scale,
             font_thickness)
 
-        points = detection.points
-        points[1][1] = points[0][1]
-        points[0][1] -= text_size[0][1]
-        points[1][0] = points[0][0] + text_size[0][0]
-        points = points.astype(int)
-        margin = 2
-        margin_top = 4
-
+        # Define tuples
+        point = namedtuple("point", ['x', 'y'])
         box = namedtuple("box", ['x_min', 'y_min', 'x_max', 'y_max'])
-        rectangle = box(points[0][0],
-                        points[0][1] - margin_top,
-                        points[1][0] + margin * 2,
-                        points[1][1] + margin_top)
+        text_dimension = namedtuple("text", ['width', 'height', 'char_count'])
 
-        test = tuple(points[0, :])
-        test2 = tuple(rectangle[:2])
+        bbox_corner = point(*(int(val) for val in bbox_location))
+        text_box = text_dimension(*text_size[0], text_size[1])
+        label_container = box(bbox_corner.x,
+                              bbox_corner.y - text_box.height - box_margin * 4,
+                              bbox_corner.x + text_box.width + box_margin * 2,
+                              bbox_corner.y)
 
         cv2.rectangle(
             img=frame,
-            pt1=tuple(rectangle[:2]),
-            pt2=tuple(rectangle[2:]),
-            color=(0, 0, 255),
+            pt1=(label_container.x_min, label_container.y_min),
+            pt2=(label_container.x_max, label_container.y_max),
+            color=box_color,
             thickness=cv2.FILLED
         )
 
         cv2.putText(
             img=frame,
             text=text,
-            org=(points[0][0] + margin, points[1][1] - 1),
+            org=(label_container.x_min + box_margin,
+                 label_container.y_max - box_margin * 2),
             fontFace=font_face,
             fontScale=font_scale,
             color=font_color,
@@ -107,15 +114,16 @@ class Tracking:
             lineType=cv2.LINE_AA,
         )
 
-    def draw(self, frame, yolo_detections, norfair_detections, tracked_objects):
+    def detection_to_tracked_linker(self, norfair_detetion, tracked_objects):
+        for object in tracked_objects:
+            if (object.last_detection == norfair_detetion):
+                return object
+
+    def draw(self, frame, norfair_detections, tracked_objects):
         # Draw detected label
         frame_scale = frame.shape[0] / 100
-        # id_size = frame_scale / 10
-        id_size = 0.5
-        # id_thickness = int(frame_scale / 3)
-        id_thickness = 1
-        pand = yolo_detections.pandas().xyxy[0]
-        length = len(pand.name)
+        id_size = frame_scale / 10
+        id_thickness = int(frame_scale / 3)
 
         # Draw detections, ids and center point/ bounding box
         if self.track_points == 'centroid':
@@ -125,25 +133,29 @@ class Tracking:
             norfair.draw_boxes(frame, norfair_detections,
                                line_width=3, draw_labels=False, label_size=id_size)
 
-        norfair.draw_tracked_objects(
-            frame, tracked_objects, id_thickness=2, id_size=id_size, color=[0, 255, 0])
+        # norfair.draw_tracked_objects(
+        #     frame, tracked_objects, id_thickness=2, id_size=id_size, color=[0, 255, 0])
 
         # Draw ROI
         cv2.polylines(frame, [np.array(self.roi_area, np.int32)], True,
                       (15, 220, 10), 6)
 
-        Point = namedtuple(
-            'Point', ['x_min', 'y_min', 'x_max', 'y_max'])
+        for detection in norfair_detections:
+            tracked_object = self.detection_to_tracked_linker(
+                detection, tracked_objects)
 
-        for obj in norfair_detections:
-            self.draw_label(
-                frame,
-                obj,
-                obj.label + " " + str(round(obj.scores[0], 2)),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                id_size,
-                (255, 255, 255),
-                id_thickness)
+            if(tracked_object != None):
+                object_id = str(tracked_object.id)
+            else:
+                object_id = "-1"
+
+            text = "id: {id} type: {label} ({conf})".format(id=object_id,
+                                                            label=detection.label,
+                                                            conf=str(round(detection.scores[0], 2)))
+
+            self.draw_label(frame,
+                            text,
+                            detection.points[0])
 
         # Draw vehicle counter
 
@@ -185,7 +197,6 @@ class Tracking:
         # Open file writer
         out = cv2.VideoWriter(
             filename=stream_location + "_processed.mkv",
-            # fourcc=int(video_stream.get(cv2.CAP_PROP_FOURCC)), # original codec from video
             fourcc=cv2.VideoWriter_fourcc(*'FMP4'),  # FFMPEG codec
             fps=video_stream.get(cv2.CAP_PROP_FPS),
             frameSize=(int(video_stream.get(cv2.CAP_PROP_FRAME_WIDTH)),
@@ -210,7 +221,6 @@ class Tracking:
             # Detect objects inside the cropped frame
             # yolo_detections = self.model(frame)
             yolo_detections = self.model(cropped_image)
-            # frame = np.squeeze(yolo_detections.render())
 
             # Convert to norfair detections
             # detections = yolo_detections_to_norfair_detections(yolo_detections, track_points=self.track_points)
@@ -225,8 +235,8 @@ class Tracking:
             self.count_objects(tracked_objects)
 
             if (self.should_draw):
-                self.draw(frame, yolo_detections, detections, tracked_objects)
-                # self.draw(cropped_image, yolo_detections, detections, tracked_objects)
+                self.draw(frame, detections, tracked_objects)
+                # self.draw(cropped_image, detections, tracked_objects)
 
             # Write to filesystem
             out.write(frame)
