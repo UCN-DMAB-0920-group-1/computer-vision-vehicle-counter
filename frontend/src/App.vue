514d3c71d9bf2fe88d87e5e4804251180fb753a6
Magnus Baggesen <baggesenmagnus@gmail.com>
2022-03-31 13:12:21 +0200

    pusher not working

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,23 +2,34 @@
   <div class="">
     <NavBar></NavBar>
     <router-view></router-view>
-        
+
     <AlertBox v-for="video in finishedVideos" :key="video.id" :video="video"></AlertBox>
   </div>
 </template>
 
 <script setup>
-  import AlertBox from "./components/core/AlertBox.vue";
-  import NavBar from "./components/core/NavBar.vue";
-  import {useStore} from "vuex"
-  const store = useStore();
+import AlertBox from "./components/core/AlertBox.vue";
+import NavBar from "./components/core/NavBar.vue";
+import Pusher from "pusher-js";
+import { useStore } from "vuex";
+import { computed } from "vue";
 
-  store.dispatch("Authorization/checkLoggedin")
+const store = useStore();
 
+store.dispatch("Authorization/checkLoggedin");
 
+let finishedVideos = computed(() => store.getters["Detections/finishedVideos"]);
 
-
-
+Pusher.logToConsole = false;
+const pusher = new Pusher(process.env.VUE_APP_PUSHER_KEY, { cluster: "eu" });
+const channel = pusher.subscribe("video-channel");
+channel.bind("video-event", function (data) {
+  console.log(data);
+  if (data.status == "Finished") {
+    store.commit("Detections/addFinishedVideo", data);
+    store.dispatch("Detections/getVideoData", { id: data.id });
+  }
+});
 </script>
 <style>
 #app {
