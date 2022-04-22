953b383ddb2ee021458f3cd7ceb8244f8a3ac1d1
Michael Andreas Graversen <michael-graversen@hotmail.com>
2022-04-22 11:19:07 +0200

    Endless call loop when run locally

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  devServer: {
-        proxy: process.env.VUE_APP_PROCESSING_ENDPOINT,
-    },
   transpileDependencies: true
 })
