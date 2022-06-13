fbef4cf0fb56ef4ca3003546e926c14dcb6f752f
nicolasmeller <nicolasmeller@hotmail.dk>
2022-06-13 11:56:53 +0200

    comments on the filehandler

--- api/application/file_handler/console_filehandler.py
+++ api/application/file_handler/console_filehandler.py
@@ -1,12 +1,17 @@
-from i_filehandler import IFileHandler
+from ..interfaces.i_filehandler import IFileHandler
+
+# use interface IFillHandler
 
 
 class ConsoleFilehandler(IFileHandler):
+    # prints out the path
     def upload(self, path, bytes):
         print("going to upload file at " + path)
+    # prints out the path and directory
 
     def download(self, dir, path):
         print("going to download file at " + dir + "\\" + path)
+    # prints what path is going to be deleted
 
     def delete(self, path):
         print("going to delete file at " + path)
