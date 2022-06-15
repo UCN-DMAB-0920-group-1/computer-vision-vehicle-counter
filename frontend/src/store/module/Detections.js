fd83b20371155fb03d29095cebd69d41aaed9d9f
Julius Bendt <julius@juto.dk>
2022-06-15 10:35:39 +0200

    Process history are sorted by date

--- frontend/src/store/module/Detections.js
+++ frontend/src/store/module/Detections.js
@@ -1,73 +1,65 @@
-import { getCookie, getPayloadValue} from "@/util/Cookie";
+import { getCookie, getPayloadValue } from "@/util/Cookie";
 
 const state = {
-    videoData: [],
-    finishedVideos: [],
-    userVideos:[],
+  videoData: [],
+  finishedVideos: [],
+  userVideos: [],
 };
 
 const mutations = {
-  setUserVideos:(state, userVideos) => (state.userVideos = userVideos),
+  setUserVideos: (state, userVideos) => (state.userVideos = userVideos),
   setVideoData: (state, videoData) => (state.videoData = videoData),
   addFinishedVideo: (state, data) => state.finishedVideos.push(data),
   removeFinishedVideoNotification: (state, id) =>
     (state.finishedVideos = state.finishedVideos.filter((item) => item.id != id)),
 };
 const actions = {
-    async getVideoData({ commit }, {id}) {
-        const response = await fetch(
-            process.env.VUE_APP_PROCESSING_ENDPOINT + "/detection/" + id, {
-                method: "GET",
-                headers: { "Authorization": getCookie("jwt") },
-            }
-        );
+  async getVideoData({ commit }, { id }) {
+    const response = await fetch(process.env.VUE_APP_PROCESSING_ENDPOINT + "/detection/" + id, {
+      method: "GET",
+      headers: { Authorization: getCookie("jwt") },
+    });
 
-        const json = await response.json();
+    const json = await response.json();
 
-        commit("setVideoData", json);
-    },
-    async downloadVideo({ commit }, file) {
-        const formData = new FormData();
-        formData.append("file", file);
+    commit("setVideoData", json);
+  },
+  async downloadVideo({ commit }, file) {
+    const formData = new FormData();
+    formData.append("file", file);
 
-        const response = await fetch(
-            process.env.VUE_APP_PROCESSING_ENDPOINT + "/detection", {
-                method: "POST",
-                body: formData,
-                headers: { "Authorization": getCookie("jwt") },
+    const response = await fetch(process.env.VUE_APP_PROCESSING_ENDPOINT + "/detection", {
+      method: "POST",
+      body: formData,
+      headers: { Authorization: getCookie("jwt") },
+    });
 
-            }
-        );
+    const json = await response.json();
+    state.videoIds.push(json.id); //Save ids for later use
 
-        const json = await response.json();
-        state.videoIds.push(json.id); //Save ids for later use
+    commit("setVideoIds", state.videoIds);
+  },
+  async downloadUserDetections({ commit }) {
+    const UUID = getPayloadValue("UUID");
+    const response = await fetch(process.env.VUE_APP_PROCESSING_ENDPOINT + "/detection/user?UUID=" + UUID, {
+      method: "GET",
+      headers: { Authorization: getCookie("jwt") },
+    });
+    const json = await response.json();
 
-        commit("setVideoIds", state.videoIds);
-    },
-    async downloadUserDetections({ commit }) {
-        const UUID = getPayloadValue("UUID")
-        const response = await fetch(
-            process.env.VUE_APP_PROCESSING_ENDPOINT + "/detection/user?UUID=" + UUID, {
-                method: "GET",
-                headers: { "Authorization": getCookie("jwt") },
-
-            }
-        );
-        const json = await response.json();
-        console.log(json);
-        commit("setUserVideos", json);
-    },
+    commit("setUserVideos", json.reverse());
+  },
 };
 const getters = {
   videoData: (state) => state.videoData,
   finishedVideos: (state) => state.finishedVideos,
-  userVideos: (state) => state.userVideos
+  userVideos: (state) => state.userVideos,
 };
 
 export default {
-    state,
-    namespaced: true,
-    mutations,
-    actions,
-    getters,
+  state,
+  namespaced: true,
+  mutations,
+  actions,
+  getters,
 };
--- frontend/src/views/UserView.vue
+++ frontend/src/views/UserView.vue
@@ -1,32 +1,28 @@
 <template>
-    <div>
-        <div v-if="loggedIn" class="flex row-reverse gap-5 flex-wrap flex-grow w-full justify-center">
-            <div class="bg-violet-300 shadow-md rounded-lg" v-for="video in videos" :key="video.UUID">
-               <DetectionResults :video="video"></DetectionResults>
-            </div>
-        </div>
-
-        <LoginText v-else></LoginText>
+  <div>
+    <div v-if="loggedIn" class="flex gap-5 flex-wrap w-full justify-center">
+      <div class="bg-violet-300 shadow-md rounded-lg" v-for="video in videos" :key="video.UUID">
+        <DetectionResults :video="video"></DetectionResults>
+      </div>
     </div>
+
+    <LoginText v-else></LoginText>
+  </div>
 </template>
 
 <script setup>
-import DetectionResults from "@/components/core/detectionResults.vue"
+import DetectionResults from "@/components/core/detectionResults.vue";
 import LoginText from "@/components/home/LoginText.vue";
 
-import {computed} from "vue"
-import {useStore} from "vuex"
+import { computed } from "vue";
+import { useStore } from "vuex";
 const store = useStore();
 let loggedIn = computed(() => store.getters["Authorization/loginState"]);
-
 
 store.dispatch("Detections/downloadUserDetections");
 
 let videos = computed(() => store.getters["Detections/userVideos"]);
-console.log(videos.value)
-
+console.log(videos.value);
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
